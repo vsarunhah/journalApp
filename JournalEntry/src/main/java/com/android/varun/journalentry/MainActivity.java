6d9ff7be8c8c18b7f81c9a2cf8d25cd7fcd763a9
Varun Shah <varunshah660@gmail.com>
2016-06-17 22:52:27 +0530

    Commented the line to be able to merge the two branches

--- JournalEntry/src/main/java/com/android/varun/journalentry/MainActivity.java
+++ JournalEntry/src/main/java/com/android/varun/journalentry/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     ListView entryList;
@@ -16,8 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, entryActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, entryActivity.class);
+//        startActivity(intent);
+
+//        whenever you use this you have to import : import android.content.Intent;
+//        and then create an intent like above and then finally startActivity(Intent). Intent intent = new Intent(context, activity you want to start)
+//        Alt + Enter (option + return on Mac) to import missing classes like android.content.Intent.
+
 
     }
 }
