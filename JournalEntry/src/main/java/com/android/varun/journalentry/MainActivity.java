599f1ccd701b6e173b771effe7c958e6c408b19a
Varun Shah <varunshah660@gmail.com>
2016-06-17 22:34:31 +0530

    Removed changing activities line in MainActivity.java

--- JournalEntry/src/main/java/com/android/varun/journalentry/MainActivity.java
+++ JournalEntry/src/main/java/com/android/varun/journalentry/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.varun.journalentry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
